464168acf5a0e0c31a9a8cbbd7d55eca86b62a4c
KharismaAditya <yenderruaditya@gmail.com>
2025-10-26 10:09:23 +0700

    tes2

--- CalculateFinalScore/src/Mainapp.java
+++ CalculateFinalScore/src/Mainapp.java
@@ -13,7 +13,7 @@ public class Mainapp {
         int jumlah;
 
         System.out.println("=== CALCULATE FINAL SCORE ===");
-        System.out.print("Enter the number of Students: "); jumlah = input.nextInt();
+        System.out.print("Insert the number of Students: "); jumlah = input.nextInt();
 
         for(int i=0; i<jumlah; i++){
             addStudent(std, i);
